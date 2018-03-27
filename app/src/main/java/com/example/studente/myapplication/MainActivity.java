7228207644a9a4961ce4d64d525b3ab362a55602
studente <alvisebruniera@maxplanck.it>
2018-03-27 09:57:25 +0200

    landscape e metodi vuoti

--- app/src/main/java/com/example/studente/myapplication/MainActivity.java
+++ app/src/main/java/com/example/studente/myapplication/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.studente.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static long op1,op2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cipherPressed(View view){
+
+    }
+
+    public void operandPressed(View view){
+
+    }
+
+    public void equalsPressed(View view){
+
+    }
+
+    public void bkspPressed(View view){
+
     }
 }
